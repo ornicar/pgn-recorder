6a6e3b5755d3bc082b570fe999e74c2fbc49a8a8
Thibault Duplessis <t@lichess.org>
2024-03-06 11:01:12 +0100

    simplify replay

--- src/replay.ts
+++ src/replay.ts
@@ -1,30 +1,24 @@
-import Router from '@koa/router';
 import Koa from 'koa';
-import * as fs from 'fs';
+import * as fs from 'fs/promises';
 
 const path = process.argv[2];
-const port = 6399;
 
-const files = await fs.promises.readdir(path);
+const files = await fs.readdir(path);
 console.log(`Found ${files.length} files in ${path}`);
 let fileIndex = 0;
 
-const app = new Koa();
-const router = new Router();
-
-router.get('/', async (ctx, _) => {
-  const file = files[fileIndex];
-  if (file) {
-    const buffer = await fs.promises.readFile(`${path}/${file}`);
-    const pgn = buffer.toString();
-    console.log(`Serving ${fileIndex + 1}/${files.length} | ${file} | size: ${pgn.length}`);
-    ctx.body = pgn;
-    fileIndex++;
-  } else {
-    console.log('No more files to serve');
-    ctx.status = 404;
-  }
-});
-
-app.use(router.routes()).use(router.allowedMethods());
-app.listen(port);
+new Koa()
+  .use(async ctx => {
+    const file = files[fileIndex];
+    if (file) {
+      const buffer = await fs.readFile(`${path}/${file}`);
+      const pgn = buffer.toString();
+      console.log(`Serving ${fileIndex + 1}/${files.length} | ${file} | size: ${pgn.length}`);
+      ctx.body = pgn;
+      fileIndex++;
+    } else {
+      console.log('No more files to serve');
+      ctx.status = 404;
+    }
+  })
+  .listen(6399);
