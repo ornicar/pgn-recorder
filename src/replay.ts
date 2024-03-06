204d9f4d6142fe170410cce925386dd87251e275
Thibault Duplessis <t@lichess.org>
2024-03-06 11:06:58 +0100

    close replayer when done

--- src/replay.ts
+++ src/replay.ts
@@ -7,7 +7,7 @@ const files = await fs.readdir(path);
 console.log(`Found ${files.length} files in ${path}`);
 let fileIndex = 0;
 
-new Koa()
+const app = new Koa()
   .use(async ctx => {
     const file = files[fileIndex];
     if (file) {
@@ -16,8 +16,12 @@ new Koa()
       console.log(`Serving ${fileIndex + 1}/${files.length} | ${file} | size: ${pgn.length}`);
       ctx.body = pgn;
       fileIndex++;
+      if (fileIndex === files.length) {
+        console.log('No more files to serve');
+        app.close();
+      }
     } else {
-      console.log('No more files to serve');
+      console.log(`File not found: ${fileIndex}`);
       ctx.status = 404;
     }
   })
