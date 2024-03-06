58294418f0c8b5f721f338e61c7641f63fb7b43d
Thibault Duplessis <t@lichess.org>
2024-03-06 10:58:25 +0100

    replay works

--- src/replay.ts
+++ src/replay.ts
@@ -1,12 +1,30 @@
 import Router from '@koa/router';
 import Koa from 'koa';
+import * as fs from 'fs';
+
+const path = process.argv[2];
+const port = 6399;
+
+const files = await fs.promises.readdir(path);
+console.log(`Found ${files.length} files in ${path}`);
+let fileIndex = 0;
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx, _) => {
-  ctx.body = 'Hello World';
+router.get('/', async (ctx, _) => {
+  const file = files[fileIndex];
+  if (file) {
+    const buffer = await fs.promises.readFile(`${path}/${file}`);
+    const pgn = buffer.toString();
+    console.log(`Serving ${fileIndex + 1}/${files.length} | ${file} | size: ${pgn.length}`);
+    ctx.body = pgn;
+    fileIndex++;
+  } else {
+    console.log('No more files to serve');
+    ctx.status = 404;
+  }
 });
 
 app.use(router.routes()).use(router.allowedMethods());
-app.listen(6399);
+app.listen(port);
